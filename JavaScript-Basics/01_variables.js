5197e3470bbfc0672654e0fdfdb1cdee1a42c677
vivek jadhav <162805017+Vivekjadhav0511@users.noreply.github.com>
2024-05-06 05:43:09 +0000

    Variables

---
+++ JavaScript-Basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 101
+
+let accountEmail = "vivekJadhav555@gmail.com"   // space Reserve in Memory With Variable
+
+var accountPassword = "55555"  // Prefer Not To use Var Because Of Issue in Functional And Block Scope 
+
+accountCity= 'Yavatmal'  // if We not Declare Variable in JavaScript Automatically Declare A Variable
+
+/* clrt + Shift + Q  */  
+
+let accountState;
+
+// accountId=5          // Const Not Changable 
+
+accountEmail="accountEmail@333"
+
+accountPassword="44444"
+
+accountCity="Mumbai"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
