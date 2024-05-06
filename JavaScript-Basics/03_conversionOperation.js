9f700c43a641685f2188aa47a799ce4adb146370
vivek jadhav <162805017+Vivekjadhav0511@users.noreply.github.com>
2024-05-06 16:28:18 +0000

    Conversion

--- JavaScript-Basics/03_conversionOperation.js
+++ JavaScript-Basics/03_conversionOperation.js
@@ -16,13 +16,63 @@ console.log(typeof score);
 
 // "33Abc" => NaN
 
-// True => 1
+// True => 1P
 
 // false => 0
 
-let isLoggedIn=" "
+let isLoggedIn="ABc "
 
 let ValueInBoolean= Boolean(isLoggedIn)  // "" => Empty String False  " abs" => True
 
 console.log(ValueInBoolean,typeof(ValueInBoolean));
+
+
+// *************************OPERATIONS****************************************
+
+let value = 3
+
+let negValue= -value
+
+console.log(negValue);  // -3
+
+let Name = 55555
+
+let ToString = String (Name)
+
+console.log(ToString,typeof ToString);
+
+console.log(2+2);
+console.log(5-2);
+console.log(2*3);
+console.log(2**3);
+
+let str1='Hello '
+
+let str2="Vivek"
+
+str3=str1 + str2
+
+console.log(str3);
+
+console.log("1"+ 5); // 15
+
+console.log(1+ "5"); //15
+
+console.log("1"+ "5");  //15
+
+console.log(+' ');
+
+let num1,num2,num3
+
+num1 = num2= num3=3+2
+
+console.log(num2);
+
+// ***********GameCounter********************
+
+let gameCounter=100;
+
+// ++gameCounter;ameCounter++;
+
+console.log(gameCounter);
 
