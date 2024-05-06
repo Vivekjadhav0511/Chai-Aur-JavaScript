12b951f819adf712c8c0ed0dc9e2341af67ed283
vivek jadhav <162805017+Vivekjadhav0511@users.noreply.github.com>
2024-05-06 16:28:39 +0000

    Comparision

---
+++ JavaScript-Basics/04_comparision.js
@@ -0,0 +1,30 @@
+console.log("2" > 1);
+
+console.log("2" < "5");  // CONVERT STRING INTO A NUMBER 
+
+console.log(5 < 2);
+                             
+console.log(null < 1);
+
+console.log(null == 0);
+
+console.log(true <= 1);  // in comparision Operator null Converted Into A 0
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);  // 3are False 
+
+
+// true
+// true
+// false
+// true
+// true
+// true
+
+
+// *********** comparision And Equlality Check Are Two Different Things * work Differently *********
+
+console.log("20" == 20);
+
+console.log("20" === 20);   // Check Equality As Well AS DataType 
