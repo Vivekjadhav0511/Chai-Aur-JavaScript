7746fdca96626a4d8c6f031720dffdd53a138026
vivek jadhav <162805017+Vivekjadhav0511@users.noreply.github.com>
2024-05-06 05:43:27 +0000

    DataTypes

---
+++ JavaScript-Basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat All js Code as Newer Version 
+
+console.log
+('Vivek')  // Code Readablity is High Priority  
+
+// ___---- PRIMATIVE DATATYPES ------___________________________________________________________________
+
+let Name="Vivek" // String " " , ' ' 
+
+let age=18;  // number
+
+let isLoggedIn=false // Boolean  true OR False
+
+// bitInt 
+
+// null => StandAlone Value   given BY User 
+
+// Undefinend => Given By Js engine
+
+// symbol => Unique
+
+console.log(Name,typeof(Name));  /// Vivek string
+
+console.log(isLoggedIn,typeof isLoggedIn);  // false boolean
+
+console.log(typeof null); // Object 
+
+console.log(typeof undefined); // Undefined  
+
+// ___---- NON-PRIMATIVE DATATYPES ------___________________________________________________________________
+
+
