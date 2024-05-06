4cb31bd01b55e470f6d0c3a0da70a0df5c95203a
vivek jadhav <162805017+Vivekjadhav0511@users.noreply.github.com>
2024-05-06 06:29:00 +0000

    typeConversion (DataTypes)

---
+++ JavaScript-Basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "30abc"
+
+// console.log(typeof score);
+
+// const {score}= req.body;
+   
+console.log(score,typeof score);  // Number 
+
+let valueInNumber=Number (score);  //  help to convert ForceFully conversion
+
+console.log(valueInNumber,typeof (valueInNumber));
+
+console.log(typeof score);
+
+// "33" => 33 Sting To Number 
+
+// "33Abc" => NaN
+
+// True => 1
+
+// false => 0
+
+let isLoggedIn=" "
+
+let ValueInBoolean= Boolean(isLoggedIn)  // "" => Empty String False  " abs" => True
+
+console.log(ValueInBoolean,typeof(ValueInBoolean));
+
