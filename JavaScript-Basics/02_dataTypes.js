e4bba54bb5824c1225b438cafa116667670e3334
vivek jadhav <162805017+Vivekjadhav0511@users.noreply.github.com>
2024-05-06 16:29:07 +0000

    Primitive DataTypes

--- JavaScript-Basics/02_dataTypes.js
+++ JavaScript-Basics/02_dataTypes.js
@@ -27,5 +27,25 @@ console.log(typeof null); // Object
 
 console.log(typeof undefined); // Undefined  
 
-// ___---- NON-PRIMATIVE DATATYPES ------___________________________________________________________________
+
+// *************** Primitibve DataTypes => { Call By Value }  it dose'nt chnage a Original Value It Passses  a Copy Of Varialbe **********************
+
+// String , Number , Boolean , null , undefined , Symbol , BigInt
+
+let Str1=55
+
+console.log(Str1);  // 55
+
+let Str2 = Str1
+
+Str2 =200
+
+console.log(Str1); // 55
+
+console.log(Str2); // 200   It Means In Primitive DataType Original Value Never get Changed 
+
+console.log(Str1);
+
+
+// ___---- NON-PRIMATIVE DATATYPES ------{ Referance }  it Allocate a Referance of value In Memory _________________
 
