c706394b82c11b4e80cc5d136a134383bd83580b
vivek jadhav <162805017+Vivekjadhav0511@users.noreply.github.com>
2024-05-17 15:47:41 +0000

    Scope

--- 03_JavaScript-basics/02_Scopes.js
+++ 03_JavaScript-basics/02_Scopes.js
@@ -10,18 +10,20 @@
 // console.table(b)
 // console.table(cc)
 
-var b = 100;
+let a = 100;  // 
 
 if (true) {  // Scope 
    let a = 10;
    var b = 20;  // var is Global Scope 
    const cc = 30;
+   console.log("INNER let 'a' : ", a); // 10 Block Scope 
 }
 
-// console.table(a)  // script.js:237 Uncaught ReferenceError: a is not defined
-console.table(b)  // 20
-console.table(cc)  //  Uncaught ReferenceError: cc is not defined
+console.log("OUTER let 'a' " ,a)  // script.js:237 Uncaught ReferenceError: a is not defined
+// console.log(b)  // 20
+// console.table(cc)  //  Uncaught ReferenceError: cc is not defined
 
+<<<<<<< HEAD
 *****************
 
    function call (){
@@ -45,3 +47,57 @@ function Fun(){
     console.log(this);
 }
 Fun()
+=======
+
+// ******NESTED SCOPE / closure ******************
+// Child function can Access the Properties / values of the Parent function is closure
+
+function one(){
+     const userName = "vivek"
+
+     function Two(){
+          const Website = "youtube"
+          console.log(userName);
+     }
+
+    //  console.log(Website); // ReferenceError: Website is not defined
+
+     Two()
+}
+
+one()
+
+
+if (true) {
+    const bookName = "How you Lost Me"
+
+    if (bookName=="How you Lost Me") {
+         console.log(bookName); // How you Lost Me 
+         const Author = " Tim Burner Lie" 
+         console.log(`${bookName} by ${Author}`); // How you Lost Me by  Tim Burner Lie
+    }
+    // console.log(Author);  // ReferenceError: Author is not defined  
+                          //beacause Author defined in Scope that we are not Able to Access the Author 
+}
+  
+// console.log(bookName); // ReferenceError: bookName is not defined
+                       //  beacause BookName defined in Scope that we are not Able to Access the BookName OutSide the Scope 
+
+// console.table(cc)  //  Uncaught ReferenceError: cc is not defined
+
+// +++++++++++++++++++++++++++++++++++++
+
+console.log(addOne(5));  // 6
+
+function addOne(num){
+    return num + 1
+}
+
+// console.log(addTwo(5)); // ReferenceError: Cannot access 'addTwo' before initialization
+
+const addTwo = function(num){
+   return num + 2              // Holding a Function in A varibale we called Function Expression 
+}
+
+console.log(addTwo(5));
+>>>>>>> 7b628a3 (Scope)
