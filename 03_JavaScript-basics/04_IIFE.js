fc94421429bfa6c224b4a4d9e4b371cc6f4a0909
vivek jadhav <162805017+Vivekjadhav0511@users.noreply.github.com>
2024-05-17 15:52:00 +0000

    IIFE

---
+++ 03_JavaScript-basics/04_IIFE.js
@@ -0,0 +1,43 @@
+
+// Immediate invoke function Expression { IIFE }
+(function IIFE(){ 
+    // Named IFFE 
+    console.log("immidiate Invoke Function Expression "); // protection Form A global Scope Polluction 
+})();
+
+
+(()=>{
+    console.log(" Arrow immidiate Invoke Function Expression 2 "); // protection Form A global Scope Polluction 
+})()
+
+
+let obj1 ={
+    userName:"vivek",
+    Price:999
+}
+console.log(obj1);
+
+let Spread=(Object.keys(obj1));
+
+console.log(...Spread);
+
+obj1.CallMe = function(){
+    console.log(`${this.userName} And Having Price ${this.Price}`);
+}
+obj1.CallMe()
+
+const {userName,Price,CallMe} = obj1
+
+console.log(userName,Price);
+
+console.log(obj1.CallMe());
+
+
+(()=>{
+    console.log("IIFE");
+})(),
+
+(function(){
+console.log("DataBase Connected");
+})()
+
