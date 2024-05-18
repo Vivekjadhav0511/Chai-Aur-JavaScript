4e6a381f18b3a20844c6befd7ea7332b43293500
Vivek Jadhav <vivekjadhav0511@gmail.com>
2024-05-18 21:11:17 +0530

    TruthyValue

---
+++ 04_controlFlow/03_truthyValue.js
@@ -0,0 +1,40 @@
+
+
+const userEmail = []
+
+
+if (userEmail) {
+    console.log("Got A user Email");
+}else{
+    console.log("Don't Have a User Email");
+}
+
+// Falsy Value => "" ,  false , 0 ,-0 , BigInt 0n , null ,undefined , NaN
+
+
+// truthy Value => {} , [] , "0" , "false" , " " , function(){} , 
+
+
+if (userEmail.length ===0) {
+    console.log("Array Is Empty");
+}else{
+    "Array IS not Empty"
+}
+
+const emptyObject = {}
+
+if (Object.keys(emptyObject).length === 0) {
+    console.log("Object Is Empty ");
+}else{
+    console.log("Object Is Not Empty");
+}
+
+
+console.log( false == 0);  // true
+
+console.log(false == ''); // true
+
+console.log(0 == ''); // true
+
+console.log(true == 0); // false 
+
