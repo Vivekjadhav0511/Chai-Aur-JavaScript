cdbd8de6e62d37b6feaaa8ce3daa485643067d46
Vivek Jadhav <vivekjadhav0511@gmail.com>
2024-05-19 21:38:17 +0530

    Do While Loop

---
+++ 05_iterations/03_doWhileLoop.js
@@ -0,0 +1,15 @@
+
+
+// DoWhile
+
+// do {
+    
+// } while (condition);
+
+let score = 11
+do {
+    console.log(" Score Is " + score); // excute first condition Check after 1st condition execuation
+    score++
+} while (score <=10)
+
+    
