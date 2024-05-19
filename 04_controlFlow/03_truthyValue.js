54b8a94713df2726bd80a9594e6eac7ac86e35fc
Vivek Jadhav <vivekjadhav0511@gmail.com>
2024-05-19 11:53:43 +0530

    nullish Coalesion

--- 04_controlFlow/03_truthyValue.js
+++ 04_controlFlow/03_truthyValue.js
@@ -38,4 +38,23 @@ console.log(0 == ''); // true
 
 console.log(true == 0); // false 
 
-// Nullish Cool
+// Nullish Coalescing Operator ( ?? ) null ,undefined 
+
+let val1
+
+// val1 = 5 ?? 10 // 5
+
+// val1 = null ?? "Nullish"  // Nullish => to handal a Null And Undefined Values 
+
+val1 = undefined ?? "Nullish " // Nullish  => to handal a Null And Undefined Values 
+
+console.log(val1);
+
+// ++++  Terniary Operator \+++++++++++++++++++++
+
+(Object.keys(emptyObject).length === 0) ? console.log("Object Is Empty") :  console.log("Object Not A Empty");
+
+const TeaPrice = 10
+
+TeaPrice == 10 ? console.log("Tea Price is 10") : console.log("Tea Price Greater Than 10");
+
