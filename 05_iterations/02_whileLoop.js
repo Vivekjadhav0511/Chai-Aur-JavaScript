62bb525ba61214e29bb6c97f1402e890f6db9fc9
Vivek Jadhav <vivekjadhav0511@gmail.com>
2024-05-19 11:54:19 +0530

    WhileLoop

---
+++ 05_iterations/02_whileLoop.js
@@ -0,0 +1,18 @@
+
+
+// while (condition) {
+    
+// }
+
+let index = 1
+
+while (index<=10) {
+    // console.log(`value of index ${index}`)
+    index = index + 2
+}
+
+
+while (index<=10) {
+    console.log(`value of index ${index}`)
+    index++
+}
