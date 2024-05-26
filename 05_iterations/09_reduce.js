1666454a5873f3fde06be2b35b90a3d166baf777
Vivek Jadhav <vivekjadhav0511@gmail.com>
2024-05-26 16:45:08 +0530

    reduce

--- 05_iterations/09_reduce.js
+++ 05_iterations/09_reduce.js
@@ -28,3 +28,61 @@ const AddWithArrow = (num1, num2=20 )=>{  // Defalt parameter
     }
 
  console.log(AddWithArrow(50));   // Nan
+
+// ++++++++++++++    REVISION   +++++++++++++++++++++
+
+ let arr = [10,20,30,40,50]
+
+ arr.forEach((val,index)=>{
+    console.log(val);
+ })
+
+ let ResultArr= arr.filter((val,index)=>{
+    return val > 20;
+
+ })
+
+ let ResultArr2= arr.map((val,index)=>{
+    return val + 1;
+
+ })
+
+
+ let ResultArr3= arr.reduce((acc,Cval)=>{
+    // console.log(`${acc} + ${Cval}`);
+    return acc + Cval ;
+
+ })
+
+ console.log(ResultArr);
+ console.log(ResultArr2);
+ console.log(ResultArr3);
+
+
+ for (let i = 0; i < arr.length; i++) {
+      console.log(`index :- ${i}  & element ${arr[i]}`);
+ }
+
+ for (const key in arr) {
+    console.log(arr[key]);
+ }
+
+ for (const val of arr) {
+    console.log(`val ${val}`);
+ }
+
+
+ let language = {
+    js : "javaScipt",
+    cpp : "C++",
+    rb:"Ruby",
+    swift:"Swift By Apple"
+}
+
+for (const lan in language) {
+ console.log(`language${lan} :- ${language[lan]}`);
+ 
+}
+
+console.log(language.hasOwnProperty("jss "));
+
