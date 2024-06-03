bcc6a51fd7b250b4c7260c26e0cbdb785b60e85a
Vivek Jadhav <vivekjadhav0511@gmail.com>
2024-06-03 21:11:58 +0530

    call

--- 10_Classes&OOP/03_call.js
+++ 10_Classes&OOP/03_call.js
@@ -2,29 +2,45 @@
 function setUserName(userName){
    this.userName=userName
    console.log("setUserName called");
+
+   this.getUserName = function(){
+      console.log(`userName is :- ${this.userName} `);
+   }
 }
 
- 
-function anotherUser(userName,email,password){
-   setUserName.call(this,userName) // to hold referance 
+// setUserName.prototype.byPrototypeFunction = function(){
+//    console.log(`userName is :- ${this.userName} byPrototypeFunction`);
+// }
 
+// setUserName.prototype.greet = function() {
+//    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+//  };
+ 
+
+ 
+function anotherUser(userName,email,password,getUserName){
+   setUserName.call(this,userName,getUserName) // to hold referance 
    this.email=email
    this.password=password
 }
 
-let userOne= new anotherUser("vivek",'js@gmail.com',88886431)
+let userOne= new anotherUser("vivek jadhav acces from another function constructor",'js@gmail.com',88886431)
+let userTwo=new setUserName("setName Acess")
 
-console.log(userOne);
+userOne.getUserName()
+userTwo.getUserName();
+// userTwo.getUserName();
+// userOne.byPrototypeFunction()
 
 
 
 
 
-function outer(){
-   function inner(){
-      console.log(this);
-   }
-   inner()
-}
+// function outer(){
+//    function inner(){
+//       console.log(this);
+//    }
+//    inner()
+// }
 
-outer()
+// outer()
